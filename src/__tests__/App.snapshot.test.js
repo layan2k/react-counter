bd228745afd97fa0def503a5c62ca94903c8640f
layan2k <lesliedeeshumba@gmail.com>
2023-11-06 01:14:57 +0200

    Fix App import issue

--- src/__tests__/App.snapshot.test.js
+++ src/__tests__/App.snapshot.test.js
@@ -1,8 +1,49 @@
 import renderer from "react-test-renderer";
 
-import App from "./App";
+import App from "../App";
 
 test("renders learn react link", () => {
 const tree = renderer.create(<App />).toJSON();
-expect(tree).toMatchInlineSnapshot();
+expect(tree).toMatchInlineSnapshot(`
+[
+  <div
+    className="Toastify"
+  />,
+  <div
+    className="sc-bdnyFh hilPsi"
+  >
+    <div
+      className="sc-gtsqUy eqvTS"
+    >
+      <button
+        className="sc-hKFymg jeNmO"
+        onClick={[Function]}
+      >
+        -
+      </button>
+      <div
+        className="sc-dlniIP hjFaNB"
+      >
+        0
+      </div>
+      <button
+        className="sc-hKFymg jeNmO"
+        onClick={[Function]}
+      >
+        +
+      </button>
+    </div>
+    <div
+      className="sc-eCAqax biHJlU"
+    >
+      <button
+        className="sc-hKFymg jeNmO"
+        onClick={[Function]}
+      >
+        Reset Button
+      </button>
+    </div>
+  </div>,
+]
+`);
 });
